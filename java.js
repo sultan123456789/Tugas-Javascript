83f8adca4f94718046a19d4fd163c41c25d1c5d0
Oktafian Sultan Hakim <43697191+sultan123456789@users.noreply.github.com>
2022-08-06 08:57:04 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,50 @@
+//soal 1
+
+var katapertama = "saya";
+var katakedua = "senang";
+var kataketiga = "belajar";
+var katakeempat = "javascript";
+
+console.log(katapertama+" "+katakedua+" "+kataketiga+" "+ katakeempat);
+
+//soal 2
+var angkapertama =Number("1");
+var angkakedua =Number("2");
+var angkaketiga =Number("3");
+var angkakeempat =Number("4");
+
+
+console.log(angkapertama+angkakedua+angkaketiga+angkakeempat);
+
+//soal 3
+var kalimat = 'wah javascript itu keren sekali';
+
+var katapertamapol = kalimat.substring(0,3);
+var katakeduapol = kalimat.substring(4,15);
+var kataketigapol = kalimat.substring(15,18);
+var katakeempatpol = kalimat.substring(19,24);
+var katakelimapol = kalimat.substring(25,31);
+
+console.log ('katapertama : ' +katapertamapol);
+console.log ('katakedua : ' +katakeduapol);
+console.log ('kataketiga : ' +kataketigapol);
+console.log ('katakeempat: ' +katakeempatpol);
+console.log ('katakelima : ' +katakelimapol);
+
+//soal 4
+var nilai;
+
+console.log('nilai >= 80' + nilai>80 + 'indeksnya A' );
+
+//soal 5
+var tanggal =13;
+console.log(tanggal.toString());
+var bulan =10;
+console.log(bulan.toString()); 
+var tahun=2000;
+console.log(tahun.toString());
+
+console.log("Saya Lahir pada "+tanggal+"/"+bulan+"/"+tahun);
+
+
+
